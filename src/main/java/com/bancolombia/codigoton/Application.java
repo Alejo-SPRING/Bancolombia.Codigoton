686edb2fec2f0d652bbd2308bdf9f2b1a9bf0c29
Alejandro557 <andersonrojas@worldofficeonline.com-at-753836939045>
2021-10-22 18:40:32 -0500

    Inicialización de proyecto.

---
+++ src/main/java/com/bancolombia/codigoton/Application.java
@@ -0,0 +1,13 @@
+package com.bancolombia.codigoton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
